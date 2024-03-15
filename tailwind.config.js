6a62d7ffdeca5f73eedb4a9a47225d7d25cc701c
Rishabh870 <67893503+Rishabh870@users.noreply.github.com>
2024-03-15 17:35:45 +0530

    changes

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,7 @@
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
-    "./src/Components/**/*.{js,ts,jsx,tsx}",
+    "./Components/**/*.{js,ts,jsx,tsx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
